17e5dd26ef18ecd6d081cb4b2f8d20a65e0d3de3
zainsheikh98 <69632062+zainsheikh98@users.noreply.github.com>
2021-06-14 17:46:09 +0500

    updated import statment of useState hook

--- src/components/CompareChoices/index.js
+++ src/components/CompareChoices/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
-import {
+import { useEffect } from "react";
   GameContainer,
   MainDivWrapper,
   Choice,
