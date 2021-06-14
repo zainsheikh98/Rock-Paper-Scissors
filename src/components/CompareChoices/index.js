dc8e8795c4d7136a0c0e5fd513dbbb6950fdf2ba
zainsheikh98 <69632062+zainsheikh98@users.noreply.github.com>
2021-06-14 18:22:14 +0500

    UPDATED IMPORTS

--- src/components/CompareChoices/index.js
+++ src/components/CompareChoices/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import{
   GameContainer,
   MainDivWrapper,
   Choice,
